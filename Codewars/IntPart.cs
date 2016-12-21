8ddf511bfcaf7a07e028e88444d0e3b11f00a3a9
Marcos Almeida <almeidamarcosmail@gmail.com>
2016-12-21 15:04:24 +0100

    IntPart

--- Codewars/IntPart.cs
+++ Codewars/IntPart.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Codewars
 {
@@ -16,19 +14,26 @@ namespace Codewars
             return string.Format("Range: {0} Average: {1:F2} Median: {2:F2}", stats.Item1, stats.Item2, stats.Item3);
         }
 
-        private static Tuple<long, float, float> ComputeStats(List<long> prod)
+        private static Tuple<long, double, double> ComputeStats(List<long> prod)
         {
             prod.Sort();
             var range = prod.Last() - prod.First();
-            var mean = (float)prod.Average();
+            var mean = prod.Average();
             var median = prod.Count % 2 == 0 ?
                             (prod[prod.Count / 2] + prod[prod.Count / 2 - 1]) / 2.0f
-                            : (float)prod[prod.Count / 2];
+                            : (double)prod[prod.Count / 2];
             return Tuple.Create(range, mean, median);
         }
 
+        private static Dictionary<long, HashSet<long>> memoizedProds = new Dictionary<long, HashSet<long>>();
+
         private static HashSet<long> ComputeProd(long n)
         {
+            if (memoizedProds.ContainsKey(n))
+            {
+                return memoizedProds[n];
+            }
+                    
             var result = new HashSet<long>();
             for(long i=1;i<n;++i)
             {
@@ -45,6 +50,7 @@ namespace Codewars
                 }
             }
             result.Add(n); // for n+0
+            memoizedProds[n] = result;
             return result;
         }
     }
@@ -54,13 +60,14 @@ namespace Codewars
     {
 
         [Test]
-        public void Test1()
+        public void TestIntPart()
         {
             Console.WriteLine("****** Basic Tests Small Numbers");
             Assert.AreEqual("Range: 5 Average: 3.50 Median: 3.50", IntPart.Part(5));
             Assert.AreEqual("Range: 1 Average: 1.50 Median: 1.50", IntPart.Part(2));
             Assert.AreEqual("Range: 2 Average: 2.00 Median: 2.00", IntPart.Part(3));
             Assert.AreEqual("Range: 3 Average: 2.50 Median: 2.50", IntPart.Part(4));
+            Assert.AreEqual("Range: 3188645 Average: 113720.82 Median: 17745.00", IntPart.Part(41));
         }
     }
 
